fc47367da520e6e968bb3a24cdf1d4edff972049
Katarzyna Wasilewska <kwasilewska2014@gmail.com>
2023-02-10 21:23:38 +0100

    add changes

--- src/orders/orders.service.ts
+++ src/orders/orders.service.ts
@@ -1,13 +1,4 @@
 import { Injectable } from '@nestjs/common';
-import { db, Order } from './../db';
 
 @Injectable()
-export class OrdersService {
-  public getAll(): Order[] {
-    return db.orders;
-  }
-
-  public getById(id: Order['id']): Order | null {
-    return db.orders.find((p) => p.id === id);
-  }
-}
+export class OrdersService {}
