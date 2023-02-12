51147465673dc9635407828d3647ebd448d549cd
Katarzyna Wasilewska <kwasilewska2014@gmail.com>
2023-02-12 14:41:34 +0100

    add changes

--- src/products/products.controller.ts
+++ src/products/products.controller.ts
@@ -3,6 +3,7 @@ import { ProductsModule } from './products.module';
 import { ProductsService } from './products.service';
 import { UpdateProductDTO } from './dto/update-product.dto';
 import { CreateProductDTO } from './dto/create-product.dto';
+import { ExternalProductDto } from './dto/external-product.dto';
 import {
   Controller,
   Get,
@@ -16,38 +17,42 @@ import {
 } from '@nestjs/common';
 
 @Controller('products')
-export class ProductsController {
-  [x: string]: any;
-  constructor(public productService: ProductsService) { }
+export class ProductsController { }
+constructor(private productsService: ProductsService) {
+  this.productsService = productsService;
+}
 
-  @Get('/:id')
-  getById(@Param('id', new ParseUUIDPipe()) id: string) {
-    const prod = this.productsService.getById(id);
-    if (!prod) throw new NotFoundException('Product not found');
-    return prod;
-  }
+@Get('/')
+getAll(): any {
+  return this.productsService.getAll();
+}
 
-  @Delete('/:id')
-  deleteById(@Param('id', new ParseUUIDPipe()) id: string) {
-    if (!this.productsService.getById(id))
-      throw new NotFoundException('Product not found');
-    this.productsService.deleteById(id);
-    return { success: true };
-  }
-  @Put('/:id')
-  update(
+@Get('/:id')
+getById(@Param('id', new ParseUUIDPipe()) id: string) {
+  const prod = this.productsService.getById(id);
+  if (!prod) throw new NotFoundException('Product not found');
+  return prod;
+}
+
+@Delete('/:id')
+deleteById(@Param('id', new ParseUUIDPipe()) id: string) {
+  if (!this.productsService.getById(id))
+    throw new NotFoundException('Product not found');
+  this.productsService.deleteById(id);
+  return { success: true };
+}
+@Put('/:id')
+update(
     @Param('id', new ParseUUIDPipe()) id: string,
     @Body() productData: UpdateProductDTO,
   ) {
-    if (!this.productsService.getById(id))
-      throw new NotFoundException('Product not found');
+  if (!this.productsService.getById(id))
+    throw new NotFoundException('Product not found');
 
-    this.productsService.updateById(id, productData);
-    return { success: true };
-  }
-  @Post('/')
-  create(@Body() productData: CreateProductDTO) {
-    return this.productsService.create(productData);
+  this.productsService.updateById(id, productData);
+  return { success: true };
 }
-
+@Post('/')
+create(@Body() productData: Omit<Product, 'id'>) {
+  return this.productsService.create(productData);
 }
